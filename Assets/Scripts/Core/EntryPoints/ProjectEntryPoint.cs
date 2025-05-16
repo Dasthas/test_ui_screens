1414fc191f0cec4fcb55752c78888a294c56c3c6
Dasthas <danyagalinskii@mail.ru>
2025-05-16 22:49:14 +0400

    update ProjectEntryPoint.cs

--- Assets/Scripts/Core/EntryPoints/ProjectEntryPoint.cs
+++ Assets/Scripts/Core/EntryPoints/ProjectEntryPoint.cs
@@ -1,12 +1,27 @@
+using Core.Settings.Table;
+using Core.UI;
 using UnityEngine;
 
 namespace Core.EntryPoints
 {
     public class ProjectEntryPoint : MonoBehaviour
     {
+        public static PrefabsTable Table;
+
+        [SerializeField] private PrefabsTable _prefabsTable;
+
+        private UIController _uiController;
+
         void Awake()
         {
-        
+            Table = _prefabsTable;
+            _uiController = new UIController();
+            _uiController.Initialize(_prefabsTable);
+        }
+
+        private void OnDestroy()
+        {
+            _uiController.Dispose();
         }
     }
 }
