ad7f279f6ed7f304838f8e7491dc95ef0fb5c0c9
Dasthas <danyagalinskii@mail.ru>
2025-05-16 22:49:51 +0400

    add button animation realization

---
+++ Assets/Scripts/Modules/UI/Buttons/SimpleAnimatedButton.cs
@@ -0,0 +1,77 @@
+using System;
+using Core.Settings.AnimatedButton;
+using Core.Settings.Screen;
+using Core.UI.AnimatedButton;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Modules.UI.Buttons
+{
+    public class SimpleAnimatedButton : BaseAnimatedButton
+    {
+        [SerializeField] private AnimatedButtonSettings _buttonAnimationSettings;
+        [SerializeField] private Button _button;
+        [SerializeField] private GraphicRaycaster _raycaster;
+        
+        public Action OnPointerDownCallback { get; set; }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            OnPointerDownCallback?.Invoke();
+            ProcessAnimation(_buttonAnimationSettings.PointerDownData);
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            ProcessAnimation(_buttonAnimationSettings.PointerUpData);
+        }
+
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            ProcessAnimation(_buttonAnimationSettings.PointerEnterData);
+        }
+
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            ProcessAnimation(_buttonAnimationSettings.PointerExitData);
+        }
+
+        public void SetRaycaster(GraphicRaycaster raycaster)
+        {
+            _raycaster = raycaster;
+        }
+
+        private void ProcessAnimation(AnimatedButtonSettingsData settings)
+        {
+            if (_raycaster == null)
+            {
+                return;
+            }
+            if (!_raycaster.enabled)
+            {
+                return;
+            }
+
+            switch (settings.SimpleAnimationType)
+            {
+                case UISimpleAnimationType.Fade:
+                    _button.image
+                        .DOFade(settings.EndValue, settings.Duration)
+                        .SetEase(settings.Ease)
+                        .SetTarget(_button)
+                        .ToUniTask(cancellationToken: gameObject.GetCancellationTokenOnDestroy());
+                    break;
+                case UISimpleAnimationType.Scale:
+                    _button.image.rectTransform
+                        .DOScale(settings.EndValue, settings.Duration)
+                        .SetEase(settings.Ease)
+                        .SetTarget(_button)
+                        .ToUniTask(cancellationToken: gameObject.GetCancellationTokenOnDestroy());
+                    break;
+            }
+        }
+    }
+}
